338c7247282adb9c993bae4b65534f9507758c38
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-01-23 10:41:08 +0100

    add new tuto

--- app/tutos/page.tsx
+++ app/tutos/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link"
 
 export default function Page (){
     return (
-        <main>
+        <main className="p-4 text-xl flex flex-col gap-6 items-center justify-center mt-12">
             <Link href={"/tutos/repasDesProfs"}>Le repas de professeurs</Link>
+            <Link href={"/tutos/transportsVoyages"}>Le transport des voyages</Link>
         </main>
     )
 }
