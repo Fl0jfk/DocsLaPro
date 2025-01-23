7248e7d401ae302d38b4d5f6d9f9c0836054bc74
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-01-23 16:28:30 +0100

    adjust design

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
             École
           </summary>
           <ul className="pl-6 py-2 space-y-2 bg-gray-50 rounded-b-lg">
-            <li className="flex gap-10">
-              <p>Récapitulatif de scolarité</p>
+            <li className="flex gap-4">
+              <p>Récapitulatif de scolarité :</p>
               <p>Version à imprimer</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Re%CC%81capitulatif+de+scolarite%CC%81.pdf"}>Version numérique</Link>
             </li>
@@ -19,43 +19,43 @@ export default function Home() {
         <details className="group bg-white rounded-lg shadow-md">
           <summary className="cursor-pointer select-none py-3 px-4 text-lg font-semibold text-gray-700 bg-gray-200 rounded-lg transition-all duration-300 group-open:rounded-b-none">Collège</summary>
           <ul className="pl-6 py-2 space-y-2 bg-gray-50 rounded-b-lg">
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Sixièmes</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Sixièmes :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Sixi%C3%A8me.pdf"}>Version à imprimer</Link>
               <p>Version numérique</p>
             </li>
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Cinquièmes</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Cinquièmes :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Cinqui%C3%A8me.pdf"}>Version à imprimer</Link>
               <p>Version numérique</p>
             </li>
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Quatrièmes</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Quatrièmes :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Quatri%C3%A8me.pdf"}>Version à imprimer</Link>
               <p>Version numérique</p>
             </li>
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Troisièmes</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Troisièmes :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Troisi%C3%A8me.pdf"}>Version à imprimer</Link>
               <p>Version numérique</p>
             </li>
-            <li className="flex gap-10">
-              <p>Récapitulatif de scolarité</p>
+            <li className="flex gap-4">
+              <p>Récapitulatif de scolarité :</p>
               <p>Version à imprimer</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Re%CC%81capitulatif+de+scolarite%CC%81.pdf"}>Version numérique</Link>
             </li>
-            <li className="flex gap-10">
-              <p>Attestation Savoir Nager</p>
+            <li className="flex gap-4">
+              <p>Attestation Savoir Nager :</p>
               <p>Version à imprimer</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Attestation+Savoir+Nager.pdf"}>Version numérique</Link>
             </li>
-            <li className="flex gap-10">
-              <p>Option Chorale</p>
+            <li className="flex gap-4">
+              <p>Option Chorale :</p>
               <p>Version à imprimer</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Option+Chorale.pdf"}>Version numérique</Link>
             </li>
-            <li className="flex gap-10">
-              <p>Option Théatre</p>
+            <li className="flex gap-4">
+              <p>Option Théatre :</p>
               <p>Version à imprimer</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Option+Th%C3%A9atre.pdf"}>Version numérique</Link>
             </li>
@@ -66,38 +66,38 @@ export default function Home() {
             Lycée
           </summary>
           <ul className="pl-6 py-2 space-y-2 bg-gray-50 rounded-b-lg">
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Secondes</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Secondes :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Seconde.pdf"}>Version à imprimer</Link>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Seconde+Num%C3%A9rique.pdf"}>Version numérique</Link>
             </li>
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Premières Générales</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Premières Générales :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Premi%C3%A8re+G%C3%A9n%C3%A9rale.pdf"}>Version à imprimer</Link>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Premie%CC%80re+Ge%CC%81ne%CC%81rale+Num%C3%A9rique.pdf"}>Version numérique</Link>
             </li>
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Premières ST2S</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Premières ST2S :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Premi%C3%A8re+ST2S.pdf"}>Version à imprimer</Link>
               <p >Version numérique</p>
             </li>
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Terminales Générales</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Terminales Générales :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Terminale+G%C3%A9n%C3%A9rale.pdf"}>Version à imprimer</Link>
               <p >Version numérique</p>
             </li>
-            <li className="flex gap-10">
-              <p>Fiche d&apos;Inscription des Terminales ST2S</p>
+            <li className="flex gap-4">
+              <p>Fiche d&apos;Inscription des Terminales ST2S :</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Fiche+Inscription+Terminale+ST2S.pdf"}>Version à imprimer</Link>
               <p >Version numérique</p>
             </li>
-            <li className="flex gap-10">
-              <p>Récapitulatif de scolarité</p>
+            <li className="flex gap-4">
+              <p>Récapitulatif de scolarité :</p>
               <p>Version à imprimer</p>
               <Link className="text-blue-500 hover:underline" href={"https://docslapro.s3.eu-west-3.amazonaws.com/Re%CC%81capitulatif+de+scolarite%CC%81.pdf"}>Version numérique</Link>
             </li>
-            <li className="flex gap-10">
-              <p>Autorisation de sortie élèves</p>
+            <li className="flex gap-4">
+              <p>Autorisation de sortie élèves :</p>
               <Link className="text-blue-500 hover:underline" href={"/documents/autorisationSortie"}>Version numérique</Link>
             </li>
           </ul>
