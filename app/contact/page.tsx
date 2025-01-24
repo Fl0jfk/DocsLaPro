fc356e0dd4bcc92ad43a75b74936198d9736e2b0
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-01-24 16:35:55 +0100

    fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import FormContact from "../components/Forms/FormContact";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-4 p-4 w-full text-xl md:pt-[10vh] sm:pt-[10vh]">
+    <main className="flex flex-col gap-4 p-4 w-full text-xl md:pt-[13vh] sm:pt-[13vh]">
         <FormContact/>
     </main>
   );
