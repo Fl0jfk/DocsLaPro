573cf6152f23d2948b349bb76f9fcf9ac6095444
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-02-04 08:43:05 +0100

    fix

--- app/stocks/page.tsx
+++ app/stocks/page.tsx
@@ -3,16 +3,18 @@
 import { useState, useEffect } from "react";
 
 export default function InventoryPage() {
-  const [inventory, setInventory] = useState<Record<string, { type: string, date_achat: string, prix_unitaire: number, quantité: number }>>({}); // Ajout de types précis
-  const [scannedCodeAdd, setScannedCodeAdd] = useState<string>(""); // Champ pour ajouter
-  const [scannedCodeRemove, setScannedCodeRemove] = useState<string>(""); // Champ pour retirer
-  const [newItem, setNewItem] = useState({ code: "", type: "", date_achat: "", prix_unitaire: 0, quantité: 0 });
+  const [inventory, setInventory] = useState<Record<string, any>>({});
+  const [scannedCodeAdd, setScannedCodeAdd] = useState<string>(""); 
+  const [scannedCodeRemove, setScannedCodeRemove] = useState<string>("");
+  const [newItem, setNewItem] = useState({ code: "", type: "", date_achat: "", prix_unitaire: 0, quantité: 0});
   const [localChanges, setLocalChanges] = useState<boolean>(false);
-  const [temporaryInventory, setTemporaryInventory] = useState<Record<string, { type: string, date_achat: string, prix_unitaire: number, quantité: number }>>({}); // Inventaire temporaire pour les changements
-  const [errorMessage, setErrorMessage] = useState<string>(""); // Message d'erreur
-  const [password, setPassword] = useState<string>(""); // Mot de passe
-  const [modificationsInProgress, setModificationsInProgress] = useState<{ code: string, type: string, quantité: number }[]>([]); // Nouveau tableau pour les modifications en cours
-
+  const [temporaryInventory, setTemporaryInventory] = useState<Record<string, any>>({});
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [password, setPassword] = useState<string>(""); 
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState<boolean>(false); 
+  const [modificationsInProgress, setModificationsInProgress] = useState<any[]>([]);
+  console.log(inventory)
+  console.log(isPasswordCorrect)
   useEffect(() => {
     fetch("/api/inventory")
       .then((res) => {
@@ -30,7 +32,6 @@ export default function InventoryPage() {
         console.error("Erreur de chargement du JSON", err);
       });
   }, []);
-
   const handleScanAdd = async (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key !== "Enter") return;
     const code = scannedCodeAdd.trim();
@@ -58,7 +59,6 @@ export default function InventoryPage() {
     setScannedCodeAdd(""); 
     setLocalChanges(true);
   };
-
   const handleScanRemove = async (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key !== "Enter") return;
     const code = scannedCodeRemove.trim();
@@ -89,28 +89,26 @@ export default function InventoryPage() {
     setScannedCodeRemove("");
     setLocalChanges(true);
   };
-
   const handleNewItemSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (newItem.code && newItem.type && newItem.date_achat && newItem.prix_unitaire) {
       const updatedInventory = { ...temporaryInventory, [newItem.code]: newItem };
       setTemporaryInventory(updatedInventory);
       setNewItem({ code: "", type: "", date_achat: "", prix_unitaire: 0, quantité: 0 });
-      setLocalChanges(true); // Indiquer qu'il y a des changements locaux
+      setLocalChanges(true);
     } else {
       console.error("Veuillez remplir tous les champs");
     }
   };
-
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
-
   const handleConfirmChanges = async () => {
     if (password === "LaProNB76240") {
+      setIsPasswordCorrect(true);
       try {
         const res = await fetch("/api/inventory", {
-          method: "PUT", // Utilisation de PUT pour mettre à jour l'inventaire
+          method: "PUT",
           headers: {
             "Content-Type": "application/json",
           },
@@ -136,7 +134,6 @@ export default function InventoryPage() {
       alert("Mot de passe incorrect");
     }
   };
-
   const exportInventory = () => {
     fetch("https://docslapro.s3.eu-west-3.amazonaws.com/Stocks/inventory.json")
       .then((response) => {
@@ -160,7 +157,6 @@ export default function InventoryPage() {
         console.error("Erreur de téléchargement :", err);
       });
   };
-
   return (
     <div className="p-4 max-w-2xl mx-auto">
       <h1 className="text-xl font-bold mb-4">Gestion de l&apos;Inventaire</h1>
@@ -206,70 +202,91 @@ export default function InventoryPage() {
           })}
         </tbody>
       </table>
-      <form onSubmit={handleNewItemSubmit} className="mb-4">
-        <h2 className="text-lg font-semibold mb-2">Ajouter un nouveau mobilier</h2>
-        <div className="space-y-2">
-          <label htmlFor="code" className="block text-sm font-medium text-gray-700">Code-barres</label>
-          <input
-            id="code"
-            type="text"
-            placeholder="Code-barres"
-            value={newItem.code}
-            onChange={(e) => setNewItem({ ...newItem, code: e.target.value })}
-            className="border p-2 w-full"
-          />
-          <label htmlFor="type" className="block text-sm font-medium text-gray-700">Type</label>
-          <input
-            id="type"
-            type="text"
-            placeholder="Type"
-            value={newItem.type}
-            onChange={(e) => setNewItem({ ...newItem, type: e.target.value })}
-            className="border p-2 w-full"
-          />
-          <label htmlFor="date_achat" className="block text-sm font-medium text-gray-700">Date d&apos;achat</label>
-          <input
-            id="date_achat"
-            type="date"
-            value={newItem.date_achat}
-            onChange={(e) => setNewItem({ ...newItem, date_achat: e.target.value })}
-            className="border p-2 w-full"
-          />
-          <label htmlFor="prix_unitaire" className="block text-sm font-medium text-gray-700">Prix unitaire (€)</label>
-          <input
-            id="prix_unitaire"
-            type="number"
-            step="0.01"
-            placeholder="Prix"
-            value={newItem.prix_unitaire}
-            onChange={(e) => setNewItem({ ...newItem, prix_unitaire: parseFloat(e.target.value) })}
-            className="border p-2 w-full"
-          />
-          <label htmlFor="quantité" className="block text-sm font-medium text-gray-700">Quantité</label>
-          <input
-            id="quantité"
-            type="number"
-            value={newItem.quantité}
-            onChange={(e) => setNewItem({ ...newItem, quantité: parseInt(e.target.value) })}
-            className="border p-2 w-full"
-          />
+      {modificationsInProgress.length > 0 && (
+        <div className="mb-4">
+          <h2 className="text-lg font-semibold">Modifications en cours</h2>
+          <table className="w-full border-collapse border border-gray-300 mb-4">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="border p-2">Code-barres</th>
+                <th className="border p-2">Type</th>
+                <th className="border p-2">Quantité</th>
+              </tr>
+            </thead>
+            <tbody>
+              {modificationsInProgress.map((mod, index) => (
+                <tr key={index} className="text-center">
+                  <td className="border p-2">{mod.code}</td>
+                  <td className="border p-2">{mod.type}</td>
+                  <td className="border p-2">{mod.date_achat}</td>
+                  <td className="border p-2">{mod.prix_unitaire}</td>
+                  <td className="border p-2">{mod.quantité}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <button type="submit" className="bg-blue-500 text-white p-2 mt-4">Ajouter l&apos;article</button>
-      </form>
-      <div className="space-x-4">
-        <button
-          onClick={handleConfirmChanges}
-          className="bg-green-500 text-white p-2"
-        >
-          Confirmer les changements
-        </button>
-        <button
-          onClick={exportInventory}
-          className="bg-gray-500 text-white p-2"
-        >
-          Exporter l&apos;inventaire
-        </button>
-      </div>
+      )}
+      <form onSubmit={handleNewItemSubmit} className="mb-4">
+  <h2 className="text-lg font-semibold mb-2">Ajouter un nouveau mobilier</h2>
+  <div className="space-y-2">
+    <div>
+      <label htmlFor="code" className="block text-sm font-medium text-gray-700">Code-barres</label>
+      <input
+        id="code"
+        type="text"
+        placeholder="Code-barres"
+        value={newItem.code}
+        onChange={(e) => setNewItem({ ...newItem, code: e.target.value })}
+        className="border p-2 w-full"
+      />
+    </div>
+    <div>
+      <label htmlFor="type" className="block text-sm font-medium text-gray-700">Type</label>
+      <input
+        id="type"
+        type="text"
+        placeholder="Type"
+        value={newItem.type}
+        onChange={(e) => setNewItem({ ...newItem, type: e.target.value })}
+        className="border p-2 w-full"
+      />
+    </div>
+    <div>
+      <label htmlFor="date_achat" className="block text-sm font-medium text-gray-700">Date d&apos;achat</label>
+      <input
+        id="date_achat"
+        type="date"
+        placeholder="Date d'achat"
+        value={newItem.date_achat}
+        onChange={(e) => setNewItem({ ...newItem, date_achat: e.target.value })}
+        className="border p-2 w-full"
+      />
+    </div>
+    <div>
+      <label htmlFor="prix_unitaire" className="block text-sm font-medium text-gray-700">Prix (€)</label>
+      <input
+        id="prix_unitaire"
+        type="number"
+        placeholder="Prix (€)"
+        value={newItem.prix_unitaire}
+        onChange={(e) => setNewItem({ ...newItem, prix_unitaire: +e.target.value })}
+        className="border p-2 w-full"
+      />
+    </div>
+  </div>
+  <button type="submit" className="mt-4 p-2 bg-green-500 text-white">Ajouter le mobilier</button>
+</form>
+
+      {localChanges && (
+        <div className="mb-4">
+          <input type="password" placeholder="Entrez le mot de passe pour valider" value={password} onChange={handlePasswordChange} className="border p-2 w-full"/>
+          <button onClick={handleConfirmChanges} className="mt-4 p-2 bg-blue-500 text-white">Valider les changements</button>
+        </div>
+      )}
+      <button onClick={exportInventory} className="mt-4 p-2 bg-yellow-500 text-white">Télécharger les stocks sur le serveur</button>
     </div>
   );
 }
+
+
