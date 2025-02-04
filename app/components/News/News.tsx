a1b32ef9d32de2bf54a12f7f3648c5b16b76d1e0
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-02-04 11:07:13 +0100

    add news ok

--- app/components/News/News.tsx
+++ app/components/News/News.tsx
@@ -9,22 +9,22 @@ export default function News() {
   return (
     <section className="p-4 flex flex-col gap-4 max-w-[1000px] mx-auto">
       <h2 className="text-4xl font-semibold">Actualités de la semaine</h2>
-      <div className="overflow-x-auto bg-white rounded-lg shadow-md">
-        <table className="min-w-full table-auto">
-          <thead className="bg-gray-100">
+      <div className="rounded-lg border-x border-b">
+        <table>
+          <thead className="bg-gray-100 rounded-t-lg">
             <tr>
-              <th className="px-4 py-2 text-left text-gray-700 border-r border-white">Jour</th>
-              <th className="px-4 py-2 text-left text-gray-700 border-r border-white">Date</th>
-              <th className="px-4 py-2 text-left text-gray-700">Actualités</th>
+              <th className="p-2 text-gray-700 border-r border-white">Jour</th>
+              <th className="p-2 text-gray-700 border-r border-white">Date</th>
+              <th className="p-2 text-gray-700">Actualités</th>
             </tr>
           </thead>
-          <tbody className="divide-y divide-gray-200">
+          <tbody className="divide-y divide-gray-700">
             {news.map((item: NewsItem) => (
                 <tr key={item.id}>
-                  <td className="px-4 py-2 border-r border-grey-100 text-4xl font-semibold">{item.jour}</td>
-                  <td className="px-4 py-2 border-r border-grey-100">{item.date}</td>
-                  <td className="px-4 py-2 ">{item.actus.map((actu) => (
-                    <p className="border-b p-4" key={actu.id}>- {actu.text}</p>
+                  <td className="p-2 border-r border-grey-100 text-4xl font-semibold">{item.jour}</td>
+                  <td className="p-2 border-r border-grey-100">{item.date}</td>
+                  <td>{item.actus.map((actu, index) => (
+                    <p className={`p-2 ${index !== item.actus.length - 1 ? 'border-b' : ''}`} key={actu.id}>- {actu.text}</p>
                   ))}</td>
                 </tr>
             ))}
