440eed42b67a42bdec28efe5741c240a412216a3
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-02-05 10:31:31 +0100

    affine page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import News from "./components/News/News";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-1 py-4 w-full text-xl md:pt-[7vh] sm:pt-[7vh]">
+    <main className="flex flex-col w-full text-xl md:pt-[7vh] sm:pt-[7vh]">
       <RollingSlider/>
       <News/>
     </main>
