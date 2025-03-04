ae39e4f02bbf2ee598e517d4dcba5184b410db6a
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-03-04 16:52:40 +0100

    delete news

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex flex-col w-full text-xl md:pt-[7vh] sm:pt-[7vh]">
       <RollingSlider/>
-      <News/>
+    
     </main>
   );
 }
