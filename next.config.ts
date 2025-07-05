8c2cb8c392965e1ce9dbcd1db64ed867611d45d4
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2025-07-05 17:22:04 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
-  default-src 'self';
+  default-src 'self' https://docslapro.s3.eu-west-3.amazonaws.com;
   connect-src 'self' https://docslapro.s3.eu-west-3.amazonaws.com https://clerk-telemetry.com https://api.clerk.dev https://clerk.dev;
-  img-src 'self' https://img.clerk.com;
+  form-action 'self' https://docslapro.s3.eu-west-3.amazonaws.com;
+  img-src 'self' https://img.clerk.com  https:;
   script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
   style-src 'self' 'unsafe-inline' https:;
   font-src 'self' https:;
 `;
+
 
 const nextConfig: NextConfig = {
   images: {
