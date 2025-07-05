208707f729ccb484192d7a190215a0a6b8f2506b
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2025-07-05 17:38:35 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,29 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  contentSecurityPolicy: {
-     
-  },
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ['/api/:path*', '/((?!_next/static|_next/image|favicon.ico).*)'],
 };
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  connect-src 'self' https://docslapro.s3.eu-west-3.amazonaws.com https://clerk-telemetry.com https://*.clerk-telemetry.com https://api.stripe.com https://maps.googleapis.com genuine-wildcat-70.clerk.accounts.dev;
+  form-action 'self' https://docslapro.s3.eu-west-3.amazonaws.com;
+  img-src 'self' https://img.clerk.com  https:;
+  script-src 'self' 'unsafe-inline' https:;
+  style-src 'self' 'unsafe-inline' https:;
+  font-src 'self' https:;
+`;
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  response.headers.set(
+    "Content-Security-Policy",
+    ContentSecurityPolicy.replace(/\n/g, "")
+  );
+  return response;
+}
--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  connect-src 'self' https://docslapro.s3.eu-west-3.amazonaws.com ...AUTRES_DOMAINE_NECESSAIRES...;
+  connect-src 'self' https://docslapro.s3.eu-west-3.amazonaws.com https://clerk-telemetry.com https://*.clerk-telemetry.com https://api.stripe.com https://maps.googleapis.com genuine-wildcat-70.clerk.accounts.dev;
   form-action 'self' https://docslapro.s3.eu-west-3.amazonaws.com;
-  img-src 'self'  https:;
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-  style-src 'self' 'unsafe-inline' https:;
+  img-src 'self' https://img.clerk.com  https:;
+  script-src 'self' 'unsafe-inline' https: http: https://*.js.stripe.com https://js.stripe.com https://maps.googleapis.com;
+  style-src 'self' 'unsafe-inline';
   font-src 'self' https:;
 `;
+
 
 const nextConfig: NextConfig = {
   images: {
