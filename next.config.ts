a2eb2fc635e2c281408c606361e7100752ef6406
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2025-07-05 17:18:54 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  connect-src 'self' https://clerk-telemetry.com https://api.clerk.dev https://clerk.dev;
+  connect-src 'self' https://docslapro.s3.eu-west-3.amazonaws.com https://clerk-telemetry.com https://api.clerk.dev https://clerk.dev;
   img-src 'self' https://img.clerk.com;
   script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
   style-src 'self' 'unsafe-inline' https:;
