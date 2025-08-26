951c39d56d26b68a038910bbf4d5143732de32e9
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-08-26 11:47:27 +0200

    fix src

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,21 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 const isDev = process.env.NODE_ENV !== "production";
+
 const ContentSecurityPolicy = `
   default-src 'self';
-  connect-src 'self' https://docslapro.s3.eu-west-3.amazonaws.com https://clerk-telemetry.com https://*.clerk-telemetry.com https://api.stripe.com https://maps.googleapis.com genuine-wildcat-70.clerk.accounts.dev;
+  connect-src 'self'
+    https://docslapro.s3.eu-west-3.amazonaws.com
+    https://clerk-telemetry.com
+    https://*.clerk-telemetry.com
+    https://api.stripe.com
+    https://maps.googleapis.com
+    https://clerk.docslapro.com
+    https://accounts.docslapro.com
+    genuine-wildcat-70.clerk.accounts.dev;
   worker-src 'self' blob:;
   form-action 'self' https://docslapro.s3.eu-west-3.amazonaws.com;
-  img-src 'self' https://img.clerk.com  https:;
+  img-src 'self' https://img.clerk.com https://clerk.docslapro.com https:;
   script-src 'self' 'unsafe-inline'${isDev ? " 'unsafe-eval'" : ""} https:;
   style-src 'self' 'unsafe-inline' https:;
   font-src 'self' https:;
@@ -17,7 +26,7 @@ export default clerkMiddleware(() => {
   const response = NextResponse.next();
   response.headers.set(
     "Content-Security-Policy",
-    ContentSecurityPolicy.replace(/\n/g, "")
+    ContentSecurityPolicy.replace(/\n/g, " ")
   );
   return response;
 });
