1424bbf9fb4853771b7311a78e3206d377988ef5
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2026-01-14 11:21:17 +0100

    fix server return null

--- app/api/google/route.ts
+++ app/api/google/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
- /* const redirectUri = encodeURIComponent(process.env.GOOGLE_REDIRECT_URI!);
+  const redirectUri = encodeURIComponent(process.env.GOOGLE_REDIRECT_URI!);
   const clientId = process.env.GOOGLE_CLIENT_ID!;
   const scope = encodeURIComponent(process.env.GOOGLE_SCOPES!);
-
   const url =
     `https://accounts.google.com/o/oauth2/v2/auth?` +
     `client_id=${clientId}` +
@@ -14,6 +13,5 @@ export async function GET() {
     `&prompt=consent` +     
     `&scope=${scope}`;
 
-  return NextResponse.redirect(url);*/
-  return NextResponse
+  return NextResponse.redirect(url);
 }
