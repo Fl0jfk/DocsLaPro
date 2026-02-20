acbc4f89a623ff5fe49bdbbe6c6311b184f959aa
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2026-02-20 10:24:46 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
+  '/',
   '/travels/(.*)/devis',
   '/api/portes-ouvertes/(.*)',
   '/api/travels/send-transport',
