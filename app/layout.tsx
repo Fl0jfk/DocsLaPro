9547f106c9afafff548c4cfb4d83ab53f691c83a
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2026-06-12 10:16:40 +0200

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
-import './globals.css';
-import ChatbotBubbleClient from "./components/ChatbotBubbleClient";
-import TenantClerkProvider from "./components/TenantClerkProvider";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body>
-        <TenantClerkProvider>
-          {children}
-          <ChatbotBubbleClient/>
-        </TenantClerkProvider>
-      </body>
-    </html>
-  );
-}
+import './globals.css';
+import ChatbotBubbleClient from "./components/ChatbotBubbleClient";
+import TenantClerkProvider from "./components/TenantClerkProvider";
+
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body>
+        <TenantClerkProvider>
+          {children}
+          <ChatbotBubbleClient/>
+        </TenantClerkProvider>
+      </body>
+    </html>
+  );
+}
